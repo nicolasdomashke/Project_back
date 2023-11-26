4a3de9f166d78c69cf91235b1bb70f9b41d4fffe
nicolasdomashke <nicolasdomashke@gmail.com>
2023-11-26 22:11:34 +0300

    Connected DB to back, working on routes

--- app/api.py
+++ app/api.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse, PlainTextResponse
+import sys
+sys.path.append("..")
+from database.bd import get_booking_info, add_booking, is_user_present, is_user_data_correct, add_user, is_event_present
 
 app = FastAPI()
 
@@ -7,6 +11,8 @@ origins = [
     "http://localhost:3000",
     "localhost:3000"
 ]
+
+user_data_global = []
 
 example = "Welcome to example route"
 
@@ -19,11 +25,72 @@ app.add_middleware(
 )
 
 
+def get_time():  # return tuple
+    return 0
+
+
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to the main page."}
 
 
-@app.get("/example", tags=["example"])
-async def get_todos() -> dict:
-    return {"data": example}
+@app.get("/booking")
+async def get_booking_table() -> dict:
+    booking_table = get_booking_info()
+    return {"data": booking_table}
+
+
+@app.post("/booking")
+async def post_booking_table(new_user: dict):
+    global user_data_global
+    if not user_data_global:
+        data = new_user["data"]
+        data.append(user_data_global[0])
+        if not is_event_present(data[0:2]):
+            add_booking(data)
+
+
+@app.post("/login")
+async def post_login(user_data: dict):
+    global user_data_global
+    data = user_data['data']
+    if is_user_present(data[0]):
+        if is_user_data_correct(data):
+            user_data_global = data
+            RedirectResponse("/")
+    #    else:
+    #        await incorrect_password()
+
+
+@app.get("/login")
+async def get_login():
+    return {"data": "Login page"}
+
+
+@app.put("/login")
+async def incorrect_password():
+    return {"data": "Wrong password!"}
+
+
+@app.post("/registration")
+async def post_registration(user_data: dict):
+    data = user_data['data']
+    if is_user_present(data[1]):
+        #await login_taken()
+        pass
+    elif False:
+        pass # email check
+    elif False:
+        pass # password check
+    else:
+        add_user(user_data)
+
+
+@app.get("/registration")
+async def get_registration():
+    return {"data": "Registration page"}
+
+
+@app.put("/registration")
+async def login_taken():
+    return {"data": "Username is already taken"}
---
+++ database/bd.py
@@ -0,0 +1,82 @@
+from sqlalchemy import MetaData, Integer, String, ForeignKey, Table, Column, Date, Time, create_engine, func
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base
+
+metadata = MetaData()
+Base = declarative_base()
+engine = create_engine('sqlite:///db.sqlite3')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    reservations = relationship('Reservation', back_populates='user')
+
+
+class Reservation(Base):
+    __tablename__ = 'reservation'
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=False)
+    time = Column(Time, nullable=False)
+    event = Column(String, nullable=False)
+    status = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='reservations')
+
+
+
+def get_booking_info():
+    booking = session.query(Reservation).all()
+    return booking
+
+
+def add_user(user):
+    new_id = session.query(func.max(User.id)).scalar() + 1
+    new_user = User(id=new_id, full_name=user[0], email=user[1], password=user[2])
+    session.add(new_user)
+    session.commit()
+
+
+def is_user_present(user_data):
+    user_with_email = session.query(User).filter_by(email=user_data).first()
+    if user_with_email:
+        return True
+    else:
+        return False
+
+
+def is_user_data_correct(user_data):
+    user_with_email = session.query(User).filter_by(email=user_data[0]).first()
+    if user_with_email:
+        return user_with_email.password == user_data[1]
+    return False
+
+
+def add_booking(new_event_data):
+    new_id = session.query(func.max(Reservation.id)).scalar() + 1
+    user_name = session.query(User).filter_by(id=new_event_data[-1]).first()
+    new_event = Reservation(id=new_id, date=new_event_data[0], time=new_event_data[1], event=new_event_data[2], status=new_event_data[3], user=user_name)
+    session.add(new_event)
+    session.commit()
+
+
+
+def is_event_present(event_data):
+    event = session.query(Reservation).filter_by(date=event_data[0]).first()
+    if event:
+        return event.time == event_data[1]
+    return False
+
+#user_to_delete = session.query(User).filter_by(id=0).first()
+#session.delete(user_to_delete)
+
+#session.commit()
+
+#users = session.query(User).all()
+
+#for user in users:
+#    print(f"User ID: {user.id}, Name: {user.full_name}, Email: {user.email}, Password: {user.password}")
